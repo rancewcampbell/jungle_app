87959f473dc912b9382abaeb4694b2832a6c8657
Rance Campbell <rancewcampbell@gmail.com>
2020-05-27 20:34:06 +0000

    add admin dashboard view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @products = Product.count()
+    @categories = Category.count()
+    puts @products.inspect
+    puts @categories.inspect
   end
 end
